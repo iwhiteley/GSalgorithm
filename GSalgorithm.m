cd85529bc605df6cebf5d5f5db79a047b28fd723
iwhiteley <i.whiteley18@imperial.ac.uk>
2019-01-29 11:47:52 +0000

    first attempt at GS function

--- GSalgorithm.m
+++ GSalgorithm.m
@@ -1,11 +1,11 @@
 %%% Gerchberg saxton
-ImageSize = [128, 128];
-
-InputField = complex(ones(ImageSize)); % Set up a uniform electric field with a phase of zero hitting the SLM.
-
-SLM = round(rand(ImageSize)*255)*2*pi/255 - pi; % Here's where the problem is - I defined the phase from 0-2pi, not -pi to pi
-hologramInput = (InputField.*exp(1i*SLM)); % Add the phase to the SLM
-
+% ImageSize = [128, 128];
+% 
+% InputField = complex(ones(ImageSize)); % Set up a uniform electric field with a phase of zero hitting the SLM.
+% 
+% SLM = round(rand(ImageSize)*255)*2*pi/255 - pi; % Here's where the problem is - I defined the phase from 0-2pi, not -pi to pi
+% hologramInput = (InputField.*exp(1i*SLM)); % Add the phase to the SLM
+% 
 %create target shape
 targetImage = complex(zeros(ImageSize));
 targetImage(20:50, 20:50) = 1+1i;
@@ -14,13 +14,13 @@ targetImage = (targetImage - mean(targetImage(:)))./std(targetImage(:));
 figure(1)
 imagesc(abs(targetImage))
 
+function x = GSalgorithm(hologramInputSLM)
+
 iteration = 0;
 TotalIterations = 100;
 Performance = zeros(1,TotalIterations);
-%partition = [0:255];
-%codebook = [-pi,pi]; %range of values to give
-partition = [-pi: 2*pi/255: pi];
-codebook = [-pi , pi];
+
+
 while (iteration < TotalIterations) 
    
     TargetPl = fftshift(fft2(hologramInput));   
@@ -33,7 +33,6 @@ while (iteration < TotalIterations)
     
     ApproxSourceAmp = ifft2(fftshift(NewTarget));
     
-    %hologram = quantiz(angle(ApproxSourceAmp), partition, codebook); %doesn't work bc AppoxSourceAmp is not real
     hologram = round(angle(ApproxSourceAmp)*256/(2*pi))/(256/(2*pi));
     
     hologramInput = (InputField.*exp(1i*hologram));
@@ -49,5 +48,5 @@ imagesc(ApproxTargetI)
 
 figure(3)
 plot(Performance)
+end
 
-%test
