909622a4f0ba7955e3a1206451c65fe644804281
iwhiteley <i.whiteley18@imperial.ac.uk>
2019-01-31 16:47:54 +0000

    function to choose between SLM and DMD

---
+++ device.m
@@ -0,0 +1,11 @@
+function hologramInputIn = device(hologramInputSLM,hologramInputDMD)
+
+if "SLM"
+    hologramInputIn = hologramInputSLM;
+elseif "DMD"
+    hologramInputIn = hologramInputDMD;
+   
+end 
+
+end
+
