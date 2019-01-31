5cb1c267ad21fabdd71663002ab346e43076b691
iwhiteley <i.whiteley18@imperial.ac.uk>
2019-01-31 15:45:13 +0000

    created script to call the GS alg and related funcs

---
+++ CallingScript.m
@@ -0,0 +1,27 @@
+%%% Calling script
+
+ImageSize = [128, 128];
+targetImage = complex(zeros(ImageSize));
+targetImage(20:50, 20:50) = 1+1i;
+targetImage = (targetImage - mean(targetImage(:)))./std(targetImage(:));
+
+figure(1)
+imagesc(abs(targetImage))
+
+% iteration = 0;
+% TotalIterations = 100;
+% Performance = zeros(1,TotalIterations);
+
+InputField = complex(ones(ImageSize)); % Set up a uniform electric field with a phase of zero hitting the SLM.
+SLM = round(rand(ImageSize)*255)*2*pi/255 - pi; 
+
+hologramInput = hologramInputSLM(SLM, InputField);
+
+[ApproxTargetI,Performance] = GSalgorithm(hologramInput,InputField, TotalIterations, targetImage);
+
+
+figure(2)
+imagesc(ApproxTargetI)
+
+figure(3)
+plot(Performance)
