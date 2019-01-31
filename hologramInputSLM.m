92c4c0d6841824e3a25e256184e1d363a477be2b
iwhiteley <i.whiteley18@imperial.ac.uk>
2019-01-31 15:47:37 +0000

    simplified and fixed function- now working

--- hologramInputSLM.m
+++ hologramInputSLM.m
@@ -1,20 +1,6 @@
-
-% 
-% function [hologramInput,targetImage] = hologramInputSLM(SLM,ImageSize)
-
-function [hologramInput] = hologramInputSLM(SLM,ImageSize)
-
-% targetImage = complex(zeros(ImageSize));
-% targetImage(20:50, 20:50) = 1+1i;
-% targetImage = (targetImage - mean(targetImage(:)))./std(targetImage(:));
-% 
-% figure(1)
-% imagesc(abs(targetImage))
-
-InputField = complex(ones(ImageSize)); % Set up a uniform electric field with a phase of zero hitting the SLM.
+%SLM
+function hologramInput = hologramInputSLM(SLM,InputField)
 
 hologramInput = (InputField.*exp(1i*SLM)); % Add the phase to the SLM
-
-
 
 end
