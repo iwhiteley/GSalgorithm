65d9fa76395200fb442862db0cf15f9c6a8e14d7
iwhiteley <i.whiteley18@imperial.ac.uk>
2019-01-31 11:40:38 +0000

    made binary dmd

--- hologramInputDMD.m
+++ hologramInputDMD.m
@@ -1,1 +1,5 @@
-function hologramInput = hologramInputDMD(DMD,
+function [hologramInput] = hologramInputDMD(ImageSize,InputField)
+
+DMD= randi([0,1],ImageSize)*2*pi - pi;
+hologramInput = (InputField.*exp(1i*DMD));
+end
