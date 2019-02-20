0f96aefbe8b6c8982bd9422896b00a872b079c1c
iwhiteley <i.whiteley18@imperial.ac.uk>
2019-02-20 14:19:02 +0000

    no longer used,instead us targetImageFunc

--- targetImage.m
+++ targetImage.m
@@ -1,5 +1,6 @@
-function targetImage = 
+function targetImage = targetImage(ImageSize)
 
 targetImage = complex(zeros(ImageSize));
 targetImage(20:50, 20:50) = 1+1i;
 targetImage = (targetImage - mean(targetImage(:)))./std(targetImage(:));
+end
