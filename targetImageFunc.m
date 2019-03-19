79022113958358782fbf5f6dce6a7c74121ec665
iwhiteley <i.whiteley18@imperial.ac.uk>
2019-03-19 16:16:09 +0000

    added new image file

--- targetImageFunc.m
+++ targetImageFunc.m
@@ -1,6 +1,10 @@
 function targetImage = targetImageFunc(ImageSize)
 
-targetImage = complex(zeros(ImageSize));
-targetImage(20:50, 20:50) = 1+1i;
-targetImage = (targetImage - mean(targetImage(:)))./std(targetImage(:));
+% targetImage = complex(zeros(ImageSize));
+% targetImage(20:50, 20:50) = 1+1i;
+% targetImage = (targetImage - mean(targetImage(:)))./std(targetImage(:));
+
+targetImage = imread('smiley2.png');
+targetImage = imresize(targetImage, ImageSize);
+targetImage = imbinarize(targetImage);
 end
