3e0ba677da4d4a6cee1c42751d029bb6f9288baf
iwhiteley <i.whiteley18@imperial.ac.uk>
2019-03-20 13:41:30 +0000

    changed and resized targetImage

--- targetImageFunc.m
+++ targetImageFunc.m
@@ -4,7 +4,15 @@ function targetImage = targetImageFunc(ImageSize)
 % targetImage(20:50, 20:50) = 1+1i;
 % targetImage = (targetImage - mean(targetImage(:)))./std(targetImage(:));
 
-targetImage = imread('smiley2.png');
-targetImage = imresize(targetImage, ImageSize);
+% targetImage = imread('smiley2.png');
+% targetImage = imresize(targetImage, ImageSize);
+% targetImage = imbinarize(targetImage);
+
+targetImage = complex(zeros(ImageSize));
+rawImage = imread('smiley2.png');
+Image = imresize(rawImage, (ImageSize/2));
+Imcomplex = uint8(zeros(size(Image)));
+complexIm= complex(Image,Imcomplex);
+targetImage(1:384,1:384) = complexIm;
 targetImage = imbinarize(targetImage);
 end
