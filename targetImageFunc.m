e40f5912bd0508c6a9d9ed32e25e3e8bf555b113
iwhiteley <i.whiteley18@imperial.ac.uk>
2019-03-22 14:28:01 +0000

    adjust to make flexible variables instead of hard coding, moved picture to argument, if statement doesn't work

--- targetImageFunc.m
+++ targetImageFunc.m
@@ -1,4 +1,4 @@
-function targetImage = targetImageFunc(ImageSize)
+function targetImage = targetImageFunc(ImageSize,raw)
 
 % targetImage = complex(zeros(ImageSize));
 % targetImage(20:50, 20:50) = 1+1i;
@@ -9,10 +9,17 @@ function targetImage = targetImageFunc(ImageSize)
 % targetImage = imbinarize(targetImage);
 
 targetImage = complex(zeros(ImageSize));
-rawImage = imread('smiley2.png');
-Image = imresize(rawImage, (ImageSize/2));
-Imcomplex = uint8(zeros(size(Image)));
+rawImage = raw;
+
+if class(rawImage) == uint8
+    Image = mean(imresize(rawImage, (ImageSize/2),3))./255;
+
+else
+    Image = mean(imresize(rawImage, (ImageSize/2),3));
+end
+
+Imcomplex = zeros(size(Image),'like',Image);
 complexIm= complex(Image,Imcomplex);
-targetImage(1:384,1:384) = complexIm;
-targetImage = imbinarize(targetImage);
+targetImage(1:size(complexIm,1),1:size(complexIm,2)) = complexIm;
+%targetImage = imbinarize(targetImage);
 end
