fef2ab744c4b9835be8f38045231b6d556b65274
iwhiteley <i.whiteley18@imperial.ac.uk>
2019-06-24 18:16:37 +0100

    added axis labels to figures

--- CallingScript.m
+++ CallingScript.m
@@ -2,13 +2,18 @@
 
 ImageSize = [768, 768];
 raw = imread('smiley2.png');
+%raw = imread('neurons_in_dish_grayscale_no_background.png');
 targetImage = targetImageFunc(ImageSize,raw);
 
+x_axis_labels={'Pixel', 'Pixel','', 'Pixel'};
+y_axis_labels = {'Pixel', 'Pixel', 'Iteration', 'Pixel'};
 
 figure;
 subplot(2,2,1);
 imagesc(abs(targetImage));
-title('targetImage')
+title('Target Image')
+xlabel('Pixel')
+ylabel('Pixel')
 
 TotalIterations = 100;
 
@@ -26,13 +31,19 @@ hologramInputIn = hologramInputDMD(DMD,InputField);
 
 subplot(2,2,2);
 imagesc(ApproxTargetI)
-title('Approx Target')
-
+title('Approximate Target')
+xlabel('Pixel')
+ylabel('Pixel')
 
 subplot(2,2,3)
 plot(Performance)
 title('Performance')
+xlabel('')
+ylabel('Iteration')
 
 subplot(2,2,4);
-imagesc(ApproxTargetI(400:600,1:100))
-title('hologram zoom')
+%imagesc(ApproxTargetI(400:600,1:100))
+imagesc(hologram)
+title('Hologram')
+xlabel('Pixel')
+ylabel('Pixel')
