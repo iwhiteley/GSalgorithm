231c222a5c08e3f4e890fd9f55238b06c195d133
iwhiteley <i.whiteley18@imperial.ac.uk>
2019-08-07 16:56:02 +0100

    removed unnecessary phase shift

--- hologramInputDMD.m
+++ hologramInputDMD.m
@@ -2,5 +2,6 @@
 function hologramInputIn = hologramInputDMD(DMD,InputField)
 
 
-hologramInputIn = (InputField.*exp(1i*DMD));
+%hologramInputIn = (InputField.*exp(1i*DMD));
+hologramInputIn = (InputField.*DMD);
 end
