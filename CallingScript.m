1b3f3cb1f580e5736b4ff09fcef276f51f65228b
iwhiteley <i.whiteley18@imperial.ac.uk>
2019-08-07 16:55:13 +0100

    added lines to change final phase hologram to amplitude post loop, changed to SLM, changed plots

--- CallingScript.m
+++ CallingScript.m
@@ -1,7 +1,8 @@
 %%% Calling script
 
 ImageSize = [768, 768];
-raw = imread('smiley2.png');
+%raw = imread('smiley.jpg');
+raw = 1-(mean(imread('smiley.jpg'),3)./255);
 %raw = imread('neurons_in_dish_grayscale_no_background.png');
 targetImage = targetImageFunc(ImageSize,raw);
 
@@ -15,36 +16,39 @@ title('Target Image')
 xlabel('Pixel')
 ylabel('Pixel')
 
-TotalIterations = 5;
+TotalIterations = 10;
 
 InputField = complex(ones(ImageSize)); % Set up a uniform electric field with a phase of zero hitting the SLM.
 
 SLM = round(rand(ImageSize)*255)*2*pi/255 - pi; 
 
-DMD = randi([0,1],ImageSize)*pi;  % DMD is now binary, 0 or pi
+%DMD = randi([0,1],ImageSize);  % DMD is now binary, 0 or pi
 %DMD = ones(ImageSize)*pi;
-hologramInputIn = hologramInputDMD(DMD,InputField);
-%hologramInputIn = hologramInputSLM(SLM,InputField);
+%hologramInputIn = hologramInputDMD(DMD,InputField);
+hologramInputIn = hologramInputSLM(SLM,InputField);
 
-[ApproxTargetI,Performance, hologram] = GSalgorithm(hologramInputIn,InputField, TotalIterations, targetImage);
-
+[ApproxTargetI,RMSE, hologram] = GSalgorithm(hologramInputIn,InputField, TotalIterations, targetImage);
+DMD = hologram>0; % HACK - logical TRUE = 1
+TargetEstimate = abs(fftshift(fft2(InputField.*DMD)));
+TargetEstimate(floor(ImageSize(1)./2)+1, floor(ImageSize(2)./2)+1) = 0;
 
 subplot(2,2,2);
-imagesc(ApproxTargetI)
+%imagesc(ApproxTargetI)
+imagesc(TargetEstimate)
 title('Approximate Target')
 xlabel('Pixel')
 ylabel('Pixel')
 
 subplot(2,2,3)
-plot(Performance)
+plot(RMSE)
 title('Performance')
 xlabel('Iteration')
-ylabel('Pixel Similarity')
-ylim([0.2,1])
+ylabel('RMSE')
+%ylim([0.2,1])
 
 subplot(2,2,4);
 %imagesc(ApproxTargetI(400:600,1:100))
-imagesc(hologram)
+imagesc(DMD)
 title('Hologram')
 xlabel('Pixel')
 ylabel('Pixel')
