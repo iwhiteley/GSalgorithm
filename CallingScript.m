cbccb84c768eb84e2036e922d8c1b4630e85c8da
iwhiteley <i.whiteley18@imperial.ac.uk>
2019-08-08 10:33:54 +0100

    no changes made

--- CallingScript.m
+++ CallingScript.m
@@ -3,7 +3,6 @@
 ImageSize = [768, 768];
 %raw = imread('smiley.jpg');
 raw = 1-(mean(imread('smiley.jpg'),3)./255);
-%raw = imread('neurons_in_dish_grayscale_no_background.png');
 targetImage = targetImageFunc(ImageSize,raw);
 
 x_axis_labels={'Pixel', 'Pixel','', 'Pixel'};
@@ -32,6 +31,8 @@ DMD = hologram>0; % HACK - logical TRUE = 1
 TargetEstimate = abs(fftshift(fft2(InputField.*DMD)));
 TargetEstimate(floor(ImageSize(1)./2)+1, floor(ImageSize(2)./2)+1) = 0;
 
+trimTargetEstimate = TargetEstimate(1:trim,1:trim);
+
 subplot(2,2,2);
 %imagesc(ApproxTargetI)
 imagesc(TargetEstimate)
@@ -44,10 +45,8 @@ plot(RMSE)
 title('Performance')
 xlabel('Iteration')
 ylabel('RMSE')
-%ylim([0.2,1])
 
 subplot(2,2,4);
-%imagesc(ApproxTargetI(400:600,1:100))
 imagesc(DMD)
 title('Hologram')
 xlabel('Pixel')
