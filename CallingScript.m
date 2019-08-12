5d2c16142c1f6cd98812c7a84512b0131fd970e7
iwhiteley <i.whiteley18@imperial.ac.uk>
2019-08-12 16:46:16 +0100

    separated plots, remved last loop, added loop

--- CallingScript.m
+++ CallingScript.m
@@ -8,7 +8,7 @@ targetImage = targetImageFunc(ImageSize,raw);
 x_axis_labels={'Pixel', 'Pixel','', 'Pixel'};
 y_axis_labels = {'Pixel', 'Pixel', 'Iteration', 'Pixel'};
 
-figure;
+figure(1);
 subplot(2,2,1);
 imagesc(abs(targetImage));
 title('Target Image')
@@ -19,12 +19,12 @@ TotalIterations = 50;
 
 InputField = complex(ones(ImageSize)); % Set up a uniform electric field with a phase of zero hitting the SLM.
 
-SLM = round(rand(ImageSize)*255)*2*pi/255 - pi; 
+SLM = round(rand(ImageSize)*255)*2*pi/255 - pi;
 
 %DMD = randi([0,1],ImageSize);  % DMD is now binary, 0 or pi
 %DMD = ones(ImageSize)*pi;
 %hologramInputIn = hologramInputDMD(DMD,InputField);
-hologramInputIn = hologramInputSLM(SLM,InputField);
+
 
 loopnum = 0;
 finalLoop = 100;
@@ -33,30 +33,34 @@ finalLoop = 100;
 % [ApproxTargetI,RMSE, hologram] = GSalgorithm(hologramInputIn,InputField, TotalIterations, targetImage);
 % DMD = hologram>0; % HACK - logical TRUE = 1
 % DMDnum = double(DMD);
-% 
+%
 % TargetEstimate = abs(fftshift(fft2(InputField.*DMDnum)));
 % TargetEstimate(floor(ImageSize(1)./2)+1, floor(ImageSize(2)./2)+1) = 0;
-% 
+%
 % loopnum = loopnum +1 ;
-% 
+%
 % trimTargetEstimate = TargetEstimate(1:trim,1:trim);
 % trimTargetEstimateNorm = (trimTargetEstimate - mean(trimTargetEstimate(:)))./std(trimTargetEstimate(:));
-% 
+%
 % RMSEtargetEst(loopnum) = sqrt(mean(((trimTargetEstimateNorm(:) - TrimtargetImageNorm(:)).^2)));
 % end
 
-[ApproxTargetI,RMSE, hologram] = GSalgorithm(hologramInputIn,InputField, TotalIterations, targetImage);
-DMD = hologram>0; % HACK - logical TRUE = 1
-DMDnum = double(DMD);
- 
-TargetEstimate = abs(fftshift(fft2(InputField.*DMDnum)));
-TargetEstimate(floor(ImageSize(1)./2)+1, floor(ImageSize(2)./2)+1) = 0;
+TotalLoops = 50;
+Performance = NaN(TotalIterations,TotalLoops);
+for index= 1:TotalLoops
+    SLM = round(rand(ImageSize)*255)*2*pi/255 - pi;
+    hologramInputIn = hologramInputSLM(SLM,InputField);
+    
+    [TargetEstimate,RMSEtargetEst, hologram] = GSalgorithm(hologramInputIn,InputField, TotalIterations, targetImage, ImageSize);
+    Performance(:,index) = RMSEtargetEst;
+end
 
-trimTargetEstimate = TargetEstimate(1:trim,1:trim);
-trimTargetEstimateNorm = (trimTargetEstimate - mean(trimTargetEstimate(:)))./std(trimTargetEstimate(:));
- 
-RMSEtargetEst = sqrt(mean(((trimTargetEstimateNorm(:) - TrimtargetImageNorm(:)).^2)));
+%% Plot results
+figure(2);
+XCoords = (1:TotalIterations)'*ones(1,TotalLoops);
+scatter(XCoords(:),Performance(:))
 
+figure(1)
 subplot(2,2,2);
 %imagesc(ApproxTargetI)
 imagesc(TargetEstimate)
@@ -65,13 +69,13 @@ xlabel('Pixel')
 ylabel('Pixel')
 
 subplot(2,2,3)
-plot(RMSE)
+plot(RMSEtargetEst)
 title('Performance')
 xlabel('Iteration')
 ylabel('RMSE')
 
 subplot(2,2,4);
-imagesc(DMD)
+imagesc(hologram)
 title('Hologram')
 xlabel('Pixel')
 ylabel('Pixel')
@@ -80,13 +84,13 @@ ylabel('Pixel')
 % plot(RMSEtargetEst)
 % title('RMSE target vs Amplitude Hol')
 % xlabel('Iteration')
-% ylabel('RMSE') 
+% ylabel('RMSE')
 
 
-RMSEvector = [RMSE,RMSEtargetEst];
-
-figure;
-plot(RMSEvector)
-title('Performance quality of hologram')
-xlabel('Iteration')
-ylabel('RMSE')
+% RMSEvector = [RMSE,RMSEtargetEst];
+%
+% figure;
+% plot(RMSEvector)
+% title('Performance quality of hologram')
+% xlabel('Iteration')
+% ylabel('RMSE')
