8195784709ae8df38c936d68334cb12208693288
iwhiteley <i.whiteley18@imperial.ac.uk>
2019-08-23 17:32:30 +0100

    remved unused lines

--- targetImageFunc.m
+++ targetImageFunc.m
@@ -1,26 +1,19 @@
 function targetImage = targetImageFunc(ImageSize,raw)
 
-% targetImage = complex(zeros(ImageSize));
-% targetImage(20:50, 20:50) = 1+1i;
-% targetImage = (targetImage - mean(targetImage(:)))./std(targetImage(:));
-
-% targetImage = imread('smiley2.png');
-% targetImage = imresize(targetImage, ImageSize);
-% targetImage = imbinarize(targetImage);
-
 targetImage = complex(zeros(ImageSize));
-rawImage = fftshift(raw);
+% rawImage = fftshift(raw);
+rawImage = raw;
 
 if strcmp(class(rawImage),'uint8')
-    Image = mean(imresize(rawImage, (ImageSize/2)),3)./255;
+    Image = mean(imresize(rawImage, (ImageSize/8)),3)./255;
 
 else
-    Image = mean(imresize(rawImage, (ImageSize/2)),3);
+    Image = mean(imresize(rawImage, (ImageSize/8)),3);
 end
 
 Imcomplex = zeros(size(Image),'like',Image);
 complexIm= complex(Image,Imcomplex);
 ImOffset = 0;
 targetImage((1:size(complexIm,1))+ImOffset,(1:size(complexIm,2))+ImOffset) = complexIm;
-%targetImage = imbinarize(targetImage);
+
 end
