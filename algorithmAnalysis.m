ad918734d953da5203892c4ec7f29b6bc3786d6a
iwhiteley <i.whiteley18@imperial.ac.uk>
2019-08-23 17:31:59 +0100

    removed plot, changed iterations numbers

--- algorithmAnalysis.m
+++ algorithmAnalysis.m
@@ -1,40 +1,35 @@
 %% data analysis
-load 5iter100loopLogo.mat
+load 100iter100loopLogo.mat
 %load timeVectorAllIters.mat
 
 %average RMSE of the final product of the algorithm
-finalIteration5logo = Performance(end, :);
-meanFinalIter5logo = mean(finalIteration5logo);
-SEMfinalIter5logo = std(finalIteration5logo)/sqrt(length(finalIteration5logo));
+finalIteration100logo = Performance(end, :);
+meanFinalIter100logo = mean(finalIteration100logo);
+SEMfinalIter100logo = std(finalIteration100logo)/sqrt(length(finalIteration100logo));
 
 % average time it takes per algorithm iteration
-averageTimePerLoop5logo = mean(ElapsedTimeVector);
-SEMtimePerLoop5logo = std(ElapsedTimeVector)/sqrt(length(ElapsedTimeVector));
-ElapsedTimeVector5logo = ElapsedTimeVector;
+averageTimePerLoop100logo = mean(ElapsedTimeVector);
+SEMtimePerLoop100logo = std(ElapsedTimeVector)/sqrt(length(ElapsedTimeVector));
+ElapsedTimeVector100logo = ElapsedTimeVector;
 
-save('analysis5iter100loopLogo.mat','finalIteration5logo', 'meanFinalIter5logo', 'SEMfinalIter5logo',...
-    'averageTimePerLoop5logo', 'SEMtimePerLoop5logo', 'ElapsedTimeVector5logo')
+save('analysis100iter100loopLogo.mat','finalIteration100logo', 'meanFinalIter100logo', 'SEMfinalIter100logo',...
+    'averageTimePerLoop100logo', 'SEMtimePerLoop100logo', 'ElapsedTimeVector100logo')
 
 %% combine files
 
-filenames = {'analysis5iter100loopLogo.mat', 'analysis10iter100loopLogo.mat', 'analysis50iter100loopLogo.mat', 'analysis100iter100loopLogo.mat',};
-for kk = 1:numel(filenames)
-    load(filenames{kk})
-end
+% filenames = {'analysis5iter100loopLogo.mat', 'analysis10iter100loopLogo.mat', 'analysis50iter100loopLogo.mat', 'analysis100iter100loopLogo.mat',};
+% for kk = 1:numel(filenames)
+%     load(filenames{kk})
+% end
 
 %% Plotting
 
-% xNames = {'5 iterations', '10 iterations', '50 iterations', '100 iterations'};
-% xpoints = (meanFinalIter5, meanFinalIter10, meanFinalIter50, meanFinalIter100);
-% ypoints = range(0.3:0.4)
-% scatter(xpoints)
-
-RMSEfinalIteration = boxplot([finalIteration5logo(:), finalIteration10logo(:), finalIteration50logo(:), finalIteration10logo(:)],'Labels', {'5 Iterations','10 Iterations','50 Iterations','100 Iterations'});
+RMSEfinalIteration = boxplot([finalIteration5logo(:), finalIteration10logo(:), finalIteration50logo(:), finalIteration100logo(:)],'Labels', {'5 Iterations','10 Iterations','50 Iterations','100 Iterations'});
 xlabel('Number of Iterations')
 ylabel('RMSE')
 title('RMSE of final Iteration')
-% 
-% AverageTimePerLoop = boxplot([ElapsedTime5iter(:), ElapsedTime10iter(:), ElapsedTime50iter(:), ElapsedTime100iter(:)], 'Labels', {'5 Iterations','10 Iterations','50 Iterations','100 Iterations'});
-% xlabel('Number of Iterations')
-% ylabel('Time (s)')
-% title('Average Time per Loop')
+
+AverageTimePerLoop = boxplot([ElapsedTimeVector5logo(:), ElapsedTimeVector10logo(:), ElapsedTimeVector50logo(:), ElapsedTimeVector100logo(:)], 'Labels', {'5 Iterations','10 Iterations','50 Iterations','100 Iterations'});
+xlabel('Number of Iterations')
+ylabel('Time (s)')
+title('Average Time per Loop')
