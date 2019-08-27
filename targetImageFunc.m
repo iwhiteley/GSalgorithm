4b19f0880e123a28d5ee84e70218f46862955d82
iwhiteley <i.whiteley18@imperial.ac.uk>
2019-08-27 20:33:21 +0100

    removed typos

--- targetImageFunc.m
+++ targetImageFunc.m
@@ -5,7 +5,7 @@ targetImage = complex(zeros(ImageSize));
 rawImage = raw;
 
 if strcmp(class(rawImage),'uint8')
-    Image = mean(imresize(rawImage, (ImageSize/2git add)),3)./255;
+    Image = mean(imresize(rawImage, (ImageSize/2)),3)./255;
 
 else
     Image = mean(imresize(rawImage, (ImageSize/2)),3);
