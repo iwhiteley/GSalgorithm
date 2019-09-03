dc22ee6c35472c94a29ea907a0442501afc10b11
iwhiteley <i.whiteley18@imperial.ac.uk>
2019-09-03 10:05:32 +0100

    added loop for metadata on average

--- CallingScript.m
+++ CallingScript.m
@@ -4,68 +4,76 @@ ImageSize = [768, 768];
 raw = 1-(mean(imread('smiley.jpg'),3)./255);
 % raw = 1-(mean(imread('Imperial_Logo.png'),3)./255);
 targetImage = targetImageFunc(ImageSize,raw);
-% load('spot64.mat');
+% load('spot96.mat');
 % targetImage = complex(spot);
-
-%  imageSizeX = 768;
-%  imageSizeY = 768;
-%  targetImage = circle(imageSizeX,imageSizeY);
-
-TotalIterations = 100;
 
 InputField = complex(ones(ImageSize)); % Set up a uniform electric field with a phase of zero hitting the SLM.
 
+TotalIterations = 5;
 TotalLoops = 100;
-Performance = NaN(TotalIterations,TotalLoops);
-ElapsedTimeVector = zeros(1,TotalLoops);
-hologramStack = zeros([ImageSize,TotalLoops]);
-imageStack = zeros([ImageSize, TotalLoops]);
-iterTimings = zeros([TotalIterations, TotalLoops]);
-% DMDnumArray = zeros(size(targetImage));
-for index= 1: TotalLoops %size(targetImage,3)
-%     tic
-    SLM = round(rand(ImageSize)*255)*2*pi/255 - pi;
-    hologramInputIn = hologramInputSLM(SLM,InputField);
-    
-   %[TargetEstimate,RMSEtargetEst, DMDnum] = GSalgorithm(hologramInputIn,InputField, TotalIterations, targetImage(:,:,index), ImageSize);
-    [ApproxTargetI,RMSEtargetEst, hologram, elapsedIterTime] = GSalgorithm(hologramInputIn,InputField, TotalIterations, targetImage, ImageSize); %phase analysis
-     %DMDnumArray(:,:,index) = DMDnum;
-    Performance(:,index) = RMSEtargetEst;
-%     hologramStack(:,:,index) = DMDnum;
-    hologramStack(:,:,index) = hologram;
-%     imageStack(:,:,index) = TargetEstimate;
-    imageStack(:,:,index) = ApproxTargetI;
-    iterTimings(:,index) = elapsedIterTime;
-    
-%     elapsed_time = toc;
-%     ElapsedTimeVector(index) = elapsed_time;
-    disp([num2str(index)]) %,' Elapsed Time = ',num2str(elapsed_time)])
+Averages = 100;
+
+MetaRMSE = zeros(TotalLoops,Averages);
+
+for in= 1: Averages
+    Performance = NaN(TotalIterations,TotalLoops);
+    ElapsedTimeVector = zeros(1,TotalLoops);
+    hologramStack = zeros([ImageSize,TotalLoops]);
+    imageStack = zeros([ImageSize, TotalLoops]);
+    iterTimings = zeros([TotalIterations, TotalLoops]);
+    RMSEaverImage = zeros(1,TotalLoops);
+    tic
+    for index= 1: TotalLoops
+        SLM = round(rand(ImageSize)*255)*2*pi/255 - pi;
+        hologramInputIn = hologramInputSLM(SLM,InputField);
+        
+        %     [ApproxTargetI,RMSEtargetEst, hologram, elapsedIterTime] = GSalgorithm(hologramInputIn,InputField, TotalIterations, targetImage, ImageSize); %phase analysis
+        [TargetEstimate,RMSEtargetEst, DMDnum] = GSalgorithm(hologramInputIn,InputField, TotalIterations, targetImage, ImageSize);
+        Performance(:,index) = RMSEtargetEst;
+        hologramStack(:,:,index) = DMDnum;
+        imageStack(:,:,index) = TargetEstimate;
+        %     hologramStack(:,:,index) = hologram; %for phase
+        %     imageStack(:,:,index) = ApproxTargetI; %for phase
+%         iterTimings(:,index) = elapsedIterTime;
+        
+        trim = size(TargetEstimate)/2;
+        trimTargetImage = targetImage(1:trim,1:trim);
+        TrimtargetImageNorm = (trimTargetImage - mean(trimTargetImage(:)))./std(trimTargetImage(:));
+        averagedImage = mean(imageStack(:,:,1:index), 3);
+        trimAverImage = averagedImage(1:trim,1:trim);
+        trimAverImageNorm = (trimAverImage - mean(trimAverImage(:)))./std(trimAverImage(:));
+        
+        RMSEaverImage(index) = sqrt(mean(((trimAverImageNorm(:) - TrimtargetImageNorm(:)).^2)));
+    end
+    MetaRMSE(:,in) = RMSEaverImage;
+    Time = toc;
+    disp([num2str(in),' time ', num2str(Time)])
 end
-XCoords = (1:TotalIterations)'*ones(1,TotalLoops);
-averagedImage = mean(imageStack, 3);
-meanIterTimings = mean(iterTimings(5,:));
+finalAveragedLogoRMSE = MetaRMSE(end, :);
+meanFinalAveragedLogoRMSE = mean(finalAveragedLogoRMSE);
+SEMfinalAveragedLogoRMSE = std(finalAveragedLogoRMSE)/sqrt(length(finalAveragedLogoRMSE));
 
-trim = [384,384];
-trimTargetImage = targetImage(1:trim,1:trim);
-TrimtargetImageNorm = (trimTargetImage - mean(trimTargetImage(:)))./std(trimTargetImage(:)); 
-
-trimAverImage = averagedImage(1:trim,1:trim);
-trimAverImageNorm = (trimAverImage - mean(trimAverImage(:)))./std(trimAverImage(:));
-
-RMSEaverImage = sqrt(mean(((trimAverImageNorm(:) - TrimtargetImageNorm(:)).^2)));
-%% Plot results
+XCoordinates = (1:TotalLoops)'*ones(1,Averages);
 figure(1);
-XCoords = (1:TotalIterations)'*ones(1,TotalLoops);
-scatter(XCoords(:),Performance(:))
-xlabel('Iteration')
+scatter(XCoordinates(:),MetaRMSE(:))
+xlabel('Averages')
 ylabel('RMSE')
-title('RMSE of 100 loops of 100 iterations')
+title('')
+save('100_100_5averagedSmiley.mat', 'MetaRMSE', 'finalAveragedLogoRMSE','meanFinalAveragedLogoRMSE', 'SEMfinalAveragedLogoRMSE', 'XCoordinates')
 
-figure(2);
-plot(ElapsedTimeVector);
-title('Time of each loop')
-xlabel('Loop')
-ylabel('time (s)')
+%% Plot results
+% figure(1);
+% XCoords = (1:TotalIterations)'*ones(1,TotalLoops);
+% scatter(XCoords(:),Performance(:))
+% xlabel('Iteration')
+% ylabel('RMSE')
+% title('RMSE of 100 loops of 100 iterations')
+
+% figure(2);
+% plot(ElapsedTimeVector);
+% title('Time of each loop')
+% xlabel('Loop')
+% ylabel('time (s)')
 
 % for index = 1:size(DMDnumArray,3)
 %     figure(1)
@@ -86,10 +94,10 @@ ylabel('time (s)')
 
 %imwrite(DMDnumArray,'ExsmallSmiley.png')
 
-%   save('5iter5loopspot64.mat', 'XCoords', 'Performance', 'iterTimings',...
+%   save('5iter100loopSpot96.mat', 'XCoords', 'Performance', 'iterTimings',...
 %       'TargetEstimate', 'hologramStack', 'imageStack', 'iterTimings', 'averagedImage', 'RMSEaverImage','meanIterTimings');
 
-% save('100iter100loopSmiley.mat', 'XCoords', 'Performance', 'iterTimings',...
+% save('100iter100loopLogoPhase.mat', 'XCoords', 'Performance', 'iterTimings',...
 %        'ApproxTargetI', 'hologramStack', 'imageStack', 'iterTimings', 'averagedImage', 'RMSEaverImage','meanIterTimings');
 
 %  save('Spot5iter100loopAveraged.mat', 'averagedImage', 'RMSEaverImage', 'XCoords', 'hologramStack', 'imageStack', 'Performance', 'iterTimings', 'meanIterTimings')
