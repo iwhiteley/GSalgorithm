776f2e3dcd5169b1619c1ab3c88811a2adc5b152
iwhiteley <i.whiteley18@imperial.ac.uk>
2019-09-25 10:46:04 +0100

    added new targetImage method

--- GScalling.m
+++ GScalling.m
@@ -1,19 +1,19 @@
 %%% Calling script
 
 ImageSize = [768, 768];
-raw = 1-(mean(imread('Imperial_Logo.png'),3)./255);
-targetImage = targetImageFunc(ImageSize,raw);
-
+% raw = 1-(mean(imread('Imperial_Logo.png'),3)./255);
+% targetImage = targetImageFunc(ImageSize,raw);
+targetImage = complex(ROI);
 InputField = complex(ones(ImageSize)); % Set up a uniform electric field with a phase of zero hitting the DMD.
 
-TotalIterations = 5;
-TotalLoops = 100;
+TotalIterations = Totaliterations;
+TotalLoops = TotalLoops;
 
     for index= 1: TotalLoops
         SLM = round(rand(ImageSize)*255)*2*pi/255 - pi;
         hologramInputIn = hologramInputSLM(SLM,InputField);
         
-        [TargetEstimate, DMDnum] = GSalgorithm(hologramInputIn,InputField, TotalIterations, targetImage, ImageSize);
+        [TargetEstimate, DMDnum] = GScode(hologramInputIn,InputField, TotalIterations, targetImage, ImageSize);
         hologramStack(:,:,index) = DMDnum;
         imageStack(:,:,index) = TargetEstimate;     
     end
